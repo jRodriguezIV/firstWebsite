662fe55b54b9a29fe6e73f3079a2743f3aa97263
jRodriguezIV <115428926+jRodriguezIV@users.noreply.github.com>
2022-12-13 05:31:16 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// function parallax_height() {
+//     var scroll_top = $(this).scrollTop();
+//     var bottom_half_top = $(".bottom-half").offset().top;
+//     var header_height = $(".header").outerHeight();
+//     $(".bottom-half").css({ "margin-top": header_height })
+//     $(".header-wrapper").css({ height: header_height - scroll_top })
+// }
+
+// parallax_height();
+// $(window).scroll(function() {
+//     parallax_height();
+// });
+// $(window).resize(function() {
+//     parallax_height()
+// })
